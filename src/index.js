41b493b67d7dede0a927ca6b85b3c5329db74ca9
Leslie Ngo <ngo.leslie@gmail.com>
2021-07-31 15:48:26 -0700

    added source for css reset and some more info in entry point

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Leslie here. This should help you get started on an app if you intend to host it on git Sites. <br/>
+ReactDOM.render(<h1>Leslie here. This should help you get started on an app if you intend to host it on git Pages. <br/>
                     The most important thing to notice is that for webpack, what normally is the "dist" folder is now renamed "docs". <br/>
-                    gitSites can only host from a folder named "docs" for some reason for now. <br/> Functionally they are equivalent. <br/>
-                    <a href="https://github.com/dootMaster/app-template-git-sites">Repo here.</a><br/>
-                    <a href="https://docs.google.com/document/d/1WPxfLVLeJL16crd7BkGZYUVbkhIF9b7G53HRJr5u1ks/edit?usp=sharing">My resume.</a>
-                    </h1>, document.getElementById('root'));
+                    git Pages can only host from a folder named "docs" for some reason for now. <br/> Functionally they are equivalent. <br/>
+                    Some other things: <br />
+                    ~~~~  .nojekyll - prevents Pages from applying site themes to your app <br />
+                    ~~~~  docs/reset.css - prevents default browser css from messing with your own css. <a href="https://meyerweb.com/eric/tools/css/reset/">Source</a>
+                    <br />
+                    ~~~~ <a href="https://github.com/dootMaster/app-template-git-pages">Repo here.</a><br/>
+                    ~~~~ <a href="https://docs.google.com/document/d/1WPxfLVLeJL16crd7BkGZYUVbkhIF9b7G53HRJr5u1ks/edit?usp=sharing">My resume.</a>
+                </h1>, document.getElementById('root'));
