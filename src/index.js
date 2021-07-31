a0fa343945439f95248b6d946aa4d40bdc30ca2c
Leslie Ngo <ngo.leslie@gmail.com>
2021-07-31 15:25:13 -0700

    added hyperlinks to the repo and my resume ;)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(<h1>Leslie here. This should help you get started on an app if you intend to host it on git Sites. <br/>
                     The most important thing to notice is that for webpack, what normally is the "dist" folder is now renamed "docs". <br/>
-                    gitSites can only host from a folder named "docs" for some reason for now. <br/> Functionally they are equivalent.
+                    gitSites can only host from a folder named "docs" for some reason for now. <br/> Functionally they are equivalent. <br/>
+                    <a href="https://github.com/dootMaster/app-template-git-sites">Repo here.</a><br/>
+                    <a href="https://docs.google.com/document/d/1WPxfLVLeJL16crd7BkGZYUVbkhIF9b7G53HRJr5u1ks/edit?usp=sharing">My resume.</a>
                     </h1>, document.getElementById('root'));
