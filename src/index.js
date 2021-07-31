20001047abd468035d44b78708f2027e517ad657
Leslie Ngo <ngo.leslie@gmail.com>
2021-07-31 15:16:02 -0700

    created all files for working template for git Sites

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Leslie here. This should help you get started on an app if you intend to host it on git Sites. <br/>
+                    The most important thing to notice is that for webpack, what normally is the "dist" folder is now renamed "docs". <br/>
+                    gitSites can only host from a folder named "docs" for some reason for now. <br/> Functionally they are equivalent.
+                    </h1>, document.getElementById('root'));
